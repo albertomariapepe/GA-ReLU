8dc1bb562c1e4ad408afac9a7fe15eb16a953698
Alberto Maria Pepe <46706022+albertomariapepe@users.noreply.github.com>
2024-02-06 12:49:12 +0100

    Add files via upload

---
+++ garelu/run.py
@@ -0,0 +1,123 @@
+import os
+
+#Clifford FNO
+
+#os.system("python3 training.py -m CF -b 32 -s 333")
+#os.system("python3 training.py -m CF -A 3 -b 32 -s 28996 -t 2080 -p 10")
+#os.system("python3 training.py -m CF -A 0 -b 32 -s 28996 -t 2080 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 28996 -t 5200 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 28996 -t 5200 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 28996 -t 10400 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 28996 -t 10400 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 28996 -t 15600 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 28996 -t 15600 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 333 -t 2080 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 333 -t 2080 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 333 -t 5200 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 333 -t 5200 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 333 -t 10400 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 333 -t 10400 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 333 -t 15600 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 333 -t 15600 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 666 -t 2080 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 666 -t 2080 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 666 -t 5200 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 666 -t 5200 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 666 -t 10400 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 666 -t 10400 -p 10")
+
+os.system("python3 training.py -m CF -A 3 -b 32 -s 666 -t 15600 -p 10")
+os.system("python3 training.py -m CF -A 0 -b 32 -s 666 -t 15600 -p 10")
+
+
+
+#os.system("python3 training.py -m CR -A 0 -b 16 -s 28996 -t 2080")
+#os.system("python3 training.py -m CR -A 1 -b 16 -s 28996 -t 2080")
+#os.system("python3 training.py -m CR -A 2 -b 16 -s 28996 -t 2080")
+
+
+
+'''
+os.system("python3 training.py -m CR -A 2 -b 16 -s 28996 -t 10400")
+os.system("python3 training.py -m CR -A 1 -b 16 -s 28996 -t 10400")
+os.system("python3 training.py -m CR -A 0 -b 16 -s 28996 -t 10400")
+
+os.system("python3 training.py -m CR -A 1 -b 16 -s 28996 -t 5200")
+os.system("python3 training.py -m CR -A 2 -b 16 -s 28996 -t 5200")
+os.system("python3 training.py -m CR -A 0 -b 16 -s 28996 -t 5200")
+
+os.system("python3 training.py -m CR -A 1 -b 16 -s 666")
+os.system("python3 training.py -m CR -A 2 -b 16 -s 666")
+
+os.system("python3 training.py -m F -b 32 -s 333")
+
+os.system("python3 training.py -m CF -b 32 -s 666")
+os.system("python3 training.py -m F -b 32 -s 666")
+
+os.system("python3 training.py -m CF -b 32 -s 999")
+os.system("python3 training.py -m F -b 32 -s 999")
+
+
+os.system("python3 training.py -m CF -b 32 -s 333 -t 2080")
+os.system("python3 training.py -m CF -b 32 -s 666 -t 2080")
+os.system("python3 training.py -m CF -b 32 -s 999 -t 2080")
+
+os.system("python3 training.py -m CF -b 32 -s 333 -t 5200")
+os.system("python3 training.py -m CF -b 32 -s 666 -t 5200")
+os.system("python3 training.py -m CF -b 32 -s 999 -t 5200")
+
+os.system("python3 training.py -m CF -b 32 -s 333 -t 10400")
+os.system("python3 training.py -m CF -b 32 -s 666 -t 10400")
+os.system("python3 training.py -m CF -b 32 -s 999 -t 10400")
+
+#standard FN0
+
+
+os.system("python3 training.py -m F -b 32 -s 333 -t 2080")
+os.system("python3 training.py -m F -b 32 -s 666 -t 2080")
+os.system("python3 training.py -m F -b 32 -s 999 -t 2080")
+
+os.system("python3 training.py -m F -b 32 -s 333 -t 5200")
+os.system("python3 training.py -m F -b 32 -s 666 -t 5200")
+os.system("python3 training.py -m F -b 32 -s 999 -t 5200")
+
+os.system("python3 training.py -m F -b 32 -s 333 -t 10400")
+os.system("python3 training.py -m F -b 32 -s 666 -t 10400")
+os.system("python3 training.py -m F -b 32 -s 999 -t 10400")
+
+######
+
+os.system("python3 training.py -m CRR -b 6 -s 333")
+os.system("python3 training.py -m CRR -b 6 -s 666")
+os.system("python3 training.py -m CRR -b 6 -s 999")
+
+os.system("python3 training.py -m CRR -b 6 -s 333 -t 10400")
+os.system("python3 training.py -m CRR -b 6 -s 666 -t 10400")
+os.system("python3 training.py -m CRR -b 6 -s 999 -t 10400")
+
+os.system("python3 training.py -m CRR -b 6 -s 333 -t 5200")
+os.system("python3 training.py -m CRR -b 6 -s 666 -t 5200")
+os.system("python3 training.py -m CRR -b 6 -s 999 -t 5200")
+
+os.system("python3 training.py -m CRR -b 6 -s 333 -t 2080")
+os.system("python3 training.py -m CRR -b 6 -s 666 -t 2080")
+os.system("python3 training.py -m CRR -b 6 -s 999 -t 2080")
+'''
+
+
+#################################
+
+
+
+
+
